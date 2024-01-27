c9a2b6dd09a84bec5c37fe8c5d877cc900d8b32d
west3005 <west3005@mail.ru>
2024-01-27 10:58:44 +0300

    Add files via upload

--- modbus/rtu/mbcrc.h
+++ modbus/rtu/mbcrc.h
@@ -27,24 +27,9 @@
  *
  */
 
-#ifndef _MB_RTU_H
-#define _MB_RTU_H
+#ifndef _MB_CRC_H
+#define _MB_CRC_H
 
-#ifdef __cplusplus
-PR_BEGIN_EXTERN_C
-#endif
-    eMBErrorCode eMBRTUInit( UCHAR slaveAddress, UCHAR ucPort, ULONG ulBaudRate,
-                             eMBParity eParity );
-void            eMBRTUStart( void );
-void            eMBRTUStop( void );
-eMBErrorCode    eMBRTUReceive( UCHAR * pucRcvAddress, UCHAR ** pucFrame, USHORT * pusLength );
-eMBErrorCode    eMBRTUSend( UCHAR slaveAddress, const UCHAR * pucFrame, USHORT usLength );
-BOOL            xMBRTUReceiveFSM( void );
-BOOL            xMBRTUTransmitFSM( void );
-BOOL            xMBRTUTimerT15Expired( void );
-BOOL            xMBRTUTimerT35Expired( void );
+USHORT          usMBCRC16( UCHAR * pucFrame, USHORT usLen );
 
-#ifdef __cplusplus
-PR_END_EXTERN_C
-#endif
 #endif
